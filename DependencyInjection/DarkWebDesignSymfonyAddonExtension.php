4990028f7afe65ed9b46a322e98bb55a42c051e5
Raymond Schouten <raymond.schouten@medicore.nl>
2017-01-28 21:55:12 +0100

    Initial commit for 2.3

---
+++ DependencyInjection/DarkWebDesignSymfonyAddonExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DarkWebDesign\SymfonyAddonBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Symfony Add-on extension.
+ *
+ * @author Raymond Schouten
+ *
+ * @since 2.3
+ */
+class DarkWebDesignSymfonyAddonExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $configs
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
