7dba8921bd9091a0b12f01400c123daf2931d3ba
Raymond Schouten <raymondschouten@users.noreply.github.com>
2021-05-24 21:31:54 +0200

    ISSUE-110: Removed hard dependency on Doctrine

--- src/DependencyInjection/DarkWebDesignSymfonyAddonExtension.php
+++ src/DependencyInjection/DarkWebDesignSymfonyAddonExtension.php
@@ -22,9 +22,7 @@ declare(strict_types=1);
 
 namespace DarkWebDesign\SymfonyAddonBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -41,7 +39,11 @@ class DarkWebDesignSymfonyAddonExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        if ($container->hasDefinition('doctrine')) {
+            $container
+                ->register(EntityType::class)
+                ->addArgument($container->getDefinition('doctrine'))
+                ->addTag('form.type');
+        }
     }
 }
