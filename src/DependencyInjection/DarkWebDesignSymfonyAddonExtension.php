9bca8778e33fafcb9c9bf159dfa1ab6c8c8dfc17
Raymond Schouten <raymondschouten@users.noreply.github.com>
2021-05-25 10:29:00 +0200

    Fixed loading service definitions

--- src/DependencyInjection/DarkWebDesignSymfonyAddonExtension.php
+++ src/DependencyInjection/DarkWebDesignSymfonyAddonExtension.php
@@ -41,7 +41,7 @@ class DarkWebDesignSymfonyAddonExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yaml');
     }
 }
