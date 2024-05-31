7dcbaa066dc78282c0b9b6ffc77f7fba5b07c33f
Raymond Schouten <raymondschouten@users.noreply.github.com>
2024-05-31 06:18:33 +0000

    #163: Updated Add-on Bundle for Symfony 7.0

--- rector.php
+++ rector.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SymfonySetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
     ])
     ->withSets([
-        LevelSetList::UP_TO_PHP_81,
-        SymfonySetList::SYMFONY_64,
+        LevelSetList::UP_TO_PHP_82,
     ])
     ->withImportNames(
         importShortClasses: false,
