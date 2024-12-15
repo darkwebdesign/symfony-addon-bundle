97a847c4e2057f852d2dfb5554adad42268f4814
Rachelle Scheijen <rachelle.scheijen@tenzinger.com>
2024-12-15 11:22:56 +0100

    Added Symfony 7.2 support

--- rector.php
+++ rector.php
@@ -10,7 +10,7 @@ return RectorConfig::configure()
         __DIR__ . '/src',
     ])
     ->withSets([
-        LevelSetList::UP_TO_PHP_82,
+        LevelSetList::UP_TO_PHP_83,
     ])
     ->withImportNames(
         importShortClasses: false,
